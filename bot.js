fa70ee7c4d8ea71cb5c2b8653be3a04837afc235
enchotikin <51168033+enchotikin@users.noreply.github.com>
2019-05-30 03:47:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,33 +13,131 @@ client.on('message', msg => {
   if (msg.author.id === ayarlar.id) return;
   if (msg.author.bot) return;
 
-  if (!msg.content.startsWith(prefix)) {
-	  return;
+  client.on("guildMemberAdd", member => {
+
+  	var channel = member.guild.channels.find("name", "gelen-giden");
+  	if (!channel) return;
+
+  	var role = member.guild.roles.find("name", "😘 Yıldırım Ailesi 😘");
+  	if (!role) return;
+
+  	member.addRole(role);
+
+  	channel.send(member + " artık " + role + " rolü ile aramızda");
+
+  	member.send("Aramıza hoş geldin! Artık @😘 Yıldırım Ailesi 😘 rolüne sahipsin!")
+
+  });
+
+  const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+    if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0x2488E7)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('Hey Sen Napıyorsun', 'Ben Sunucu Botuyum Lütfen Beni Sunucunda Dene.')
+    return message.author.sendEmbed(ozelmesajuyari); }
+
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.channel.send('Birşey Yazmalısınız');
+
+  message.delete();
+
+  console.log(`Duyuru: "${message.author.username}#${message.author.discriminator}" "${mesaj}"`);
+
+      const mesajat = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setDescription('' + mesaj + '')
+
+      client.users.forEach(u => {
+u.sendEmbed(mesajat)
+})
+
+message.channel.send(`:white_check_mark: Mesaj basariyla **` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + `** kullanıcıya gonderildi.`);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['duyurla'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'duyur',
+  description: 'İstediğiniz şeyi bota duyurtur.',
+  usage: 'duyuru [duyurmak istediğiniz şey]'
+};
+
+const Discord = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('⚠ **|** Üzgünüm, komutu kullanmak için yetkiniz yok.')
+ if (!message.guild) {
+  const ozelmesajuyari = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setTimestamp()
+  .setAuthor(message.author.username, message.author.avatarURL)
+  .addField(':warning: Uyarı :warning:', 'temizle komutunu özel mesajlarda kullanılamaz!')}
+  let mesajsayisi = parseInt(args.join(' '));
+  if (!mesajsayisi) return message.channel.send(' **|** Kaç mesaj sileceğimi belirtmedin!?')
+ // if (mesajsayisi.length < 1) return message.channel.send(' **|** Kaç mesaj sileceğimi belirtmedin!?')
+  if (mesajsayisi > 101) return message.channel.send(' **|** En fazla **100** mesaj silebilirim.');
+  message.channel.bulkDelete(mesajsayisi)
+  message.channel.send('<a:heart:535357007444836352>  **|** Tamamdır, **' + mesajsayisi + '** adet mesaj sildim.')
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sil'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'temizle',
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'temizle <temizlenecek mesaj sayısı>'
+};
+
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+	let mesaj = args.slice(0).join(' ');
+	if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
+  message.delete();
+  message.channel.send(mesaj);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['say', 'söyle'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'yaz',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'yaz [yazdırmak istediğiniz şey]'
+};
+
+
+
+  client.on('guildBanAdd' , (guild, user) => {
+    let Banyiyenler = guild.channels.find('name', 'ban-yiyenler');
+    if (!Banyiyenler) return;
+    Banyiyenler.send('https://media.giphy.com/media/8njotXALXXNrW/giphy.gif **Adalet dağıtma zamanı gelmiş!**  '+ user.username +  ' **Aileden Kovuldun Güle Güle** :fist: :writing_hand:  :spy:' );
+  });
+
+  client.on('message', msg => {
+    if (msg.content.toLowerCase() === 'sa') {
+        msg.reply(' :heart: Aleyküm Selam Yıldırım Ailesine Hoşgeldin :heart: !');
   }
-  if (msg.content.toLowerCase() === prefix + 'ping') {
-    msg.reply('Pong! **' + client.ping + '** ms');
-  }
-  if (msg.content.toLowerCase() === prefix + 'sa') {
-    msg.reply('Aleyküm selam Yıldırım Ailesine Tekrardan Hoşgeldin!');
-  }
-  if (msg.content.toLowerCase() === prefix + 'yaz') {
-    msg.delete();
-    msg.channel.sendMessage(msg.content);
-  }
-  if (msg.content.toLowerCase() === prefix + 'temizle') {
-    msg.channel.bulkDelete(100);
-    msg.channel.sendMessage("100 adet mesaj silindi! ");
-  }
-  if (msg.content.toLowerCase() === prefix + 'reboot') {
-    if (msg.author.id !== ayarlar.sahip) {
-      msg.reply('Benim yapımcım değilsin!');
-    } else {
-      msg.channel.sendMessage(`Bot yeniden başlatılıyor...`).then(msg => {
-      console.log(`BOT: Bot yeniden başlatılıyor...`);
-      process.exit(0);
-    })
-   }
-  }
-});
+  });
+
 
 client.login(process.env.BOT_TOKEN);
