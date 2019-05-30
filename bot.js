cfc8a8ab971a2e6e9364a3498db14a632f0e3553
enchotikin <51168033+enchotikin@users.noreply.github.com>
2019-05-30 04:01:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,23 +9,6 @@ client.on('ready', () => {
 });
 
 
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
-
-module.exports.run = async (bot, message, args) => {
-
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send(":x: Dostum bir sayı belirtmelisin.");
-  message.channel.bulkDelete(args[0]).then(() => {
-    message.channel.send(`:white_check_mark: Başarı ile ${args[0]} adet mesaj silindi.`).then(msg => msg.delete(5000));
-  });
-}
-
-module.exports.help = {
-  name: "temizle"
-}
-
-
 client.on('message', msg => {
   console.log(`LOG: S: ${msg.guild.name} M: ${msg.content} Y: ${msg.author.tag}`);
   if (msg.author.id === ayarlar.id) return;
